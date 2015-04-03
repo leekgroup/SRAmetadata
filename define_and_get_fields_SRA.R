70f3767e1d7d01b6953ded683e9e2ace8276f5f8
lcolladotor <lcollado@jhsph.edu>
2015-04-03 15:20:52 -0400

    Improve code readability
    * Used paste0() instead of paste(sep = "")
    * Use <- for assign operations, = for arguments
    * Use 4 blank spaces instead of 2 for tabs
    * Make it readable in 80 char lines when possible
    * Broke long single lines of code into more readable ones. See if(){}else{} case

--- define_and_get_fields_SRA.R
+++ define_and_get_fields_SRA.R
@@ -2,7 +2,7 @@
 library('RSQLite')
 
 # Define functions
-"%p%"  <- function(x, y) paste(x, y, sep = "")
+"%p%"  <- function(x, y) paste0(x, y)
 
 # Update sql if necessary:
 sra.meta <- file.path('..', 'SRAmetadb.sqlite') # Using the file from another location to avoid having copies of this 2.4 Gb file
@@ -89,32 +89,42 @@ selected <- dbGetQuery(sra_con, query)
 print("Consumed time of query:")
 proc.time() - timeStart
 
-query<- paste("SELECT run_accession, file_name, md5, bytes,sradb_updated FROM fastq WHERE run_accession IN ('", 
-               paste(selected$run_accession, collapse="', '"), "')", sep = "")
+query <- paste0("SELECT run_accession, file_name, md5, bytes,sradb_updated FROM fastq WHERE run_accession IN ('", 
+    paste(selected$run_accession, collapse="', '"), "')")
 fastq_namefiles <- dbGetQuery(sra_con, query)
 
 metadata <- merge(selected, fastq_namefiles, by = "run_accession")
 
-get.fastq.urls <- function(df){
-  url <- rep(NA, length(df$run_accession))
-  for(i in 1:length(df$run_accession)) {
-    run <- df$run_accession[i]
-    filename <- df$file_name[i]
-    if(nchar(run) < 10) {
-      url[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq', substring(run, 1, 6), run, filename)
-    } else {
-      dir2 <- paste( c(rep(x='0', 12-nchar(run)), substring(run, 10, nchar(run)) ), collapse = '' )
-      url[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq', substring(run, 1, 6), dir2, run, filename)
-    }  
-  }
-  return(url)
+get.fastq.urls <- function(df) {
+    url <- rep(NA, length(df$run_accession))
+    for(i in 1:length(df$run_accession)) {
+        run <- df$run_accession[i]
+        filename <- df$file_name[i]
+        if(nchar(run) < 10) {
+            url[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq',
+                substring(run, 1, 6), run, filename)
+        } else {
+        dir2 <- paste( c(rep(x='0', 12-nchar(run)), substring(run, 10, 
+            nchar(run))), collapse = '' )
+        url[i] <- file.path('ftp://ftp.sra.ebi.ac.uk/vol1/fastq', 
+            substring(run, 1, 6), dir2, run, filename)
+        }  
+    }
+    return(url)
 }
 metadata$URL <- get.fastq.urls(metadata)
 
-search.field <- function(column, field){
-  r <- paste0('.*\\|?\\|?', field,': (.*?) \\|?\\|.*|.*\\|?\\|?', field, ': (.*?)\\|?\\|?.*?')
-  res <- sub(r, "\\1", column, perl = TRUE, ignore.case = TRUE)
-  unlist(lapply(res, function(x) if(grepl(".*\\|\\|.*", x) == TRUE){return(NA)} else{return(x)}))
+search.field <- function(column, field) {
+    r <- paste0('.*\\|?\\|?', field,': (.*?) \\|?\\|.*|.*\\|?\\|?', field,
+        ': (.*?)\\|?\\|?.*?')
+    res <- sub(r, "\\1", column, perl = TRUE, ignore.case = TRUE)
+    unlist(lapply(res, function(x) 
+        if(grepl(".*\\|\\|.*", x) == TRUE){
+            return(NA)
+        } else{
+            return(x)
+        })
+    )
 }
 
 # Get population
@@ -144,19 +154,21 @@ primary_key_name = "meta_id"
 fields <- sub("\\.", "__", colnames(metadata))
 
 ids <- paste(fields[1:4], " varchar(255) not NULL,", sep = "", collapse = " ")
-registers <- paste(fields[5:length(fields)], "longtext default 'NA'", sep = " ", collapse = ", ")
+registers <- paste(fields[5:length(fields)], "longtext default 'NA'", 
+    sep = " ", collapse = ", ")
 
 query <- "CREATE TABLE " %p% table_name %p% "(" %p% 
-  primary_key_name %p% " int unsigned auto_increment not NULL," %p%
-  ids %p% registers %p% ");"
+    primary_key_name %p% " int unsigned auto_increment not NULL," %p%
+    ids %p% registers %p% ");"
 
 dbGetQuery(meta_con, query)
 
-for(i in 1:nrow(metadata)){
-  x <- unlist(gsub("'", "prime", metadata[i,], fixed = TRUE))
-  values <- paste("'" %p% x, "'", sep = "", collapse = ",")
-  query = "INSERT INTO " %p%  table_name %p% " VALUES (" %p% '"' %p% i %p% '"' %p% "," %p% values %p% ");"
-  dbGetQuery(meta_con, query)
+for(i in 1:nrow(metadata)) {
+    x <- unlist(gsub("'", "prime", metadata[i,], fixed = TRUE))
+    values <- paste("'" %p% x, "'", sep = "", collapse = ",")
+    query <- "INSERT INTO " %p%  table_name %p% " VALUES (" %p% '"' %p%
+        i %p% '"' %p% "," %p% values %p% ");"
+    dbGetQuery(meta_con, query)
 }
 
 dbDisconnect(sra_con)
