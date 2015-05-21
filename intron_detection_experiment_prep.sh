5eeb813fe5afed90fef08753f3fe3a01f86922f9
Abhinav Nellore <anellore@gmail.com>
2015-05-21 16:35:46 -0400

    changes some directory names

--- intron_detection_experiment_prep.sh
+++ intron_detection_experiment_prep.sh
@@ -2,7 +2,7 @@
 ### Preprocesses 3000 SRA samples to obtain introns and their "initial" coverage across samples
 ### Should be run on Rail-RNA v0.1.6
 ### Uses manifest file from version of SRAmetadata repo tagged "intron_detection_experiment"
-OUTDIR=s3://rail-eu-west-1/SRA.3000.prepped
+OUTDIR=s3://rail-eu-west-1/SRA3kprepped
 SRAMETADATAREPO=/Users/eterna/SRAmetadata # Location of SRA metadata repo. Its HEAD should be at "intron_detection_experiment".
 
 rail-rna prep elastic -m $SRAMETADATAREPO/sample_manifest_file/sample_size_3000.txt -o $OUTDIR -c 20 --region eu-west-1 --master-instance-type c3.2xlarge --core-instance-type c3.2xlarge --no-consistent-view --master-instance-bid-price 0.11 --core-instance-bid-price 0.11 --ec2-key-name raileuwest1 --do-not-check-manifest --do-not-bin-quals --skip-bad-records
