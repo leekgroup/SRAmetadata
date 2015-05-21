1dfa4f965f2cf84b0f2b007b5fd541fec7218795
Abhinav Nellore <anellore@gmail.com>
2015-05-21 12:44:38 -0400

    preps for 3000 run

--- intron_detection_experiment_align.sh
+++ intron_detection_experiment_align.sh
@@ -2,8 +2,8 @@
 ### Collections introns across 3000 SRA samples
 ### Should be run on version of Rail-RNA repo tagged "intron_detection_experiment"
 ### Uses manifest file from version of SRAmetadata repo tagged "intron_detection_experiment"
-INDIR=s3://rail-eu-west-1/SRA3000prepped
-OUTDIR=s3://rail-eu-west-1/collectintrons3000
+INDIR=s3://rail-eu-west-1/SRA.3000.prepped
+OUTDIR=s3://rail-eu-west-1/collectintrons.3000
 SRAMETADATAREPO=/Users/eterna/SRAmetadata # Location of SRA metadata repo. Its HEAD should be at "intron_detection_experiment".
 
-rail-rna align elastic -m $SRAMETADATAREPO/sample_manifest_file/sample_size_3000.txt -o $OUTDIR -i $INDIR -a hg19 --region eu-west-1 -c 60 --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge --core-instance-bid-price 0.35 --master-instance-bid-price 0.35 --no-consistent-view --deliverables itn,idx --ec2-key-name raileuwest1 --do-not-check-manifest
+rail-rna align elastic -m $SRAMETADATAREPO/sample_manifest_file/sample_size_3000.txt -o $OUTDIR -i $INDIR -a hg19 --region eu-west-1 -c 60 --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge --core-instance-bid-price 0.35 --master-instance-bid-price 0.35 --no-consistent-view --deliverables itn,idx,tsv,bw --ec2-key-name raileuwest1 --do-not-check-manifest
