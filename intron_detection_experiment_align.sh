78e16c5602d1a9909b851ed7df3618cf28342ed0
Abhinav Nellore <anellore@gmail.com>
2015-06-06 17:30:35 -0400

    preps for another shot at obtaining an isofrag index

--- intron_detection_experiment_align.sh
+++ intron_detection_experiment_align.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 ### Collections introns across 3000 SRA samples
-### Should be run on Rail-RNA v0.1.6a
+### Should be run on Rail-RNA v0.1.7
 ### Uses manifest file from version of SRAmetadata repo tagged "intron_detection_experiment"
 INDIR=s3://rail-eu-west-1/SRA3kp
-OUTDIR=s3://rail-eu-west-1/SRA3ka4
+OUTDIR=s3://rail-eu-west-1/SRA3kat
 SRAMETADATAREPO=/Users/eterna/SRAmetadata # Location of SRA metadata repo. Its HEAD should be at "intron_detection_experiment".
 
-python ~/rail/src align elastic -m $SRAMETADATAREPO/sample_manifest_file/sample_size_3000.txt -o $OUTDIR -i $INDIR -a hg19 --region eu-west-1 -c 1500 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge --core-instance-bid-price 0.13 --master-instance-bid-price 0.13 --no-consistent-view --deliverables itn,idx --max-task-attempts 6 --ec2-key-name raileuwest1 --json
+python ~/rail/src align elastic -m $SRAMETADATAREPO/sample_manifest_file/sample_size_3000.txt -o $OUTDIR -i $INDIR -a hg19 --region eu-west-1 -c 250 --core-instance-type c3.8xlarge --master-instance-type c3.8xlarge --core-instance-bid-price 0.60 --master-instance-bid-price 0.60 --no-consistent-view --deliverables itn,idx --intron-criteria 0.03,-1 --max-task-attempts 6 --ec2-key-name raileuwest1
