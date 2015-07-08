5ab4b544c22e38c99e6fbf3c811f820bdd8b4937
Abhinav Nellore <anellore@gmail.com>
2015-07-08 17:00:54 -0400

    tweaks load script

--- sample_manifest_file/multiple_sample_manifest_file_script/redis_commands.sh
+++ sample_manifest_file/multiple_sample_manifest_file_script/redis_commands.sh
@@ -8,6 +8,6 @@ MAWK=/scratch0/langmead-fs1/sraintrons/mawk-1.3.4-20150503/mawk
 PYTHON=pypy
 
 $MAWK -F ',' '{print "zadd numsamples " (NF+1)/2 " " NR}' $SRAINTRONS/all_SRA_introns.tsv | $PYTHON proto.py | $REDIS/redis-cli --pipe --pipe-timeout 0
-cut -f8 $SRAINTRONS/all_SRA_introns.tsv | $MAWK -F ',' '{maxval=0;for(i=1;i<=NF;i++){if($i>maxval){maxval=$i;}} print "zadd maxcoverage " maxval " " NR}' $SRAINTRONS/all_SRA_introns.tsv | $PYTHON proto.py | $REDIS/redis-cli --pipe --pipe-timeout 0
+cut -f8 $SRAINTRONS/all_SRA_introns.tsv | $MAWK -F ',' '{maxval=$1;for(i=2;i<=NF;i++){if($i>maxval){maxval=$i;}} print "zadd maxcoverage " maxval " " NR}' $SRAINTRONS/all_SRA_introns.tsv | $PYTHON proto.py | $REDIS/redis-cli --pipe --pipe-timeout 0
 cut -f8 $SRAINTRONS/all_SRA_introns.tsv | $MAWK -F ',' '{minval=$1;for(i=2;i<=NF;i++){if($i<minval){minval=$i;}} print "zadd mincoverage " minval " " NR}' $SRAINTRONS/all_SRA_introns.tsv | $PYTHON proto.py | $REDIS/redis-cli --pipe --pipe-timeout 0
 $MAWK '{print "hmset " NR " chrom " $1 " start " $2 " end " $3 " strand " $4 " motif " $5$6}' $SRAINTRONS/all_SRA_introns.tsv | $PYTHON proto.py | $REDIS/redis-cli --pipe --pipe-timeout 0
