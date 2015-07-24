c0a2f7625c4603dacd79a2800194626e9fbfdad8
joseah <joseah@lcg.unam.mx>
2015-07-23 22:49:14 -0400

    Write results to file

--- population/build_countmatrix_sra_introns.py
+++ population/build_countmatrix_sra_introns.py
@@ -14,18 +14,19 @@ reads = []
 n = 21504
 
 file = open(tsv, 'r')
+output = open("countmatrix_introns.txt", 'w')
 for l in file:
     l = l.strip('\n').split('\t')
     reads = map(int, l[7].split(','))
     if max(reads) >= 5 and float(sum(reads))/len(reads) > 0.95:
-    	all_sample_reads = ['0'] * n
-    	indexes = map(int, l[6].split(','))
-    	j = 0
-    	for i in indexes:
-    		all_sample_reads[i] = reads[j]
-    		j = j + 1
-    	print l[0] + "_" + l[1] + "_" + l[2] + "_" + "("+ l[3]+ ")",
-    	for r in all_sample_reads:
-    		print "\t" + str(r),
-    	print ""
+        all_sample_reads = ['0'] * n
+        indexes = map(int, l[6].split(','))
+        j = 0
+        for i in indexes:
+            all_sample_reads[i] = reads[j]
+            j = j + 1
+        print >> output, l[0] + "_" + l[1] + "_" + l[2] + "_" + "("+ l[3]+ ")",
+        for r in all_sample_reads:
+            print >> output,"\t" + str(r),
+        print >> output, ""
 file.close()
