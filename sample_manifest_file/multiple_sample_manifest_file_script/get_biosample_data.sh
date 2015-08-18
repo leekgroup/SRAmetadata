d12f8c683cc69f10ef1706206fff385568b08e06
Abhinav Nellore <anellore@gmail.com>
2015-08-18 14:49:05 -0400

    adds sra sample data from ncbi biosample database

---
+++ sample_manifest_file/multiple_sample_manifest_file_script/get_biosample_data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Creates a table with sample metadata to facilitate manual construction of a tidier table.
+# Requires curl, php, and index_to_sra_accession.tsv
+for samp in $(cut -f3 index_to_SRA_accession.tsv | sort | uniq); do curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=biosample&id=$(curl "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=biosample&term=$samp" | sed -n 's|[^<]*<Id>\([^<]*\)</Id>[^<]*|\1|gp')" | grep '<SampleData>' | php -r 'while(($line=fgets(STDIN)) !== FALSE) echo html_entity_decode($line, ENT_QUOTES|ENT_HTML401);' | awk -v mysamp=$samp '{print mysamp "\t" $0}'; done >biosample_data_to_parse.tsv
