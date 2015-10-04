86b22e00d97e8c8c1f714751dbac7d7a94c58fe3
Abhinav Nellore <anellore@gmail.com>
2015-10-04 18:30:22 -0400

    fixes chr issue

--- sra_vs_ann.py
+++ sra_vs_ann.py
@@ -94,6 +94,8 @@ if __name__ == '__main__':
             tokens = line.strip().split('\t')
             tokens[1] = str(int(tokens[1]) + 2)
             tokens[2] = str(int(tokens[2]))
+            if tokens[0].startswith('chr'):
+                tokens[0] = 'chr' + tokens[0]
             if tokens[0] in refs:
                 annotated_junctions.add(tuple(tokens[:-1]))
         extract_process.stdout.close()
